6a4dd80bb3c460c0efc2b65ded6ca190973cc164
Shrushti <shrushtidhokchaule@gmail.com>
2026-03-05 10:27:52 +0530

    folder structure created

---
+++ client/src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from './Companents/Navbar';
+import Footer from './Companents/Footer';
+import Home from "./Pages/Home";
+import Rooms from "./Pages/Room";
+import RoomDetails from "./Pages/RoomDetail";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+
+
+function App() {
+  return (
+    
+    <>
+    <Router>
+             <Navbar></Navbar>
+      <Routes>
+ 
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/register" element={<Register></Register>}></Route>
+        <Route path="/room" element={<Rooms></Rooms>}></Route>
+        <Route path="/roomdetail" element={<RoomDetails></RoomDetails>}></Route>
+        
+    
+      </Routes>
+          <Footer></Footer>
+    </Router>
+    </>
+  )
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+const PORT=5000;
+app.get("/", (req, res) => {
+  res.send("RoomForU API Running");
+});
+
+app.listen(PORT, () => {
+  console.log("Server running on port 5000");
+});
