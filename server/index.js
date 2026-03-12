e90b92d85385426ff032fd81e7ce5792d3597c75
siddhikharde <siddhikharde2005@gmail.com>
2026-03-12 18:45:28 +0530

    register aname,

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import User from "./models/Users.js";
+import bcrypt from "bcryptjs"
 
 dotenv.config();
 const app = express();
@@ -18,12 +20,42 @@ app.get("/", (req, res) => {
   res.send("RoomForU API Running");
 });
 
-app.post("/register", (req, res) => {
+app.post("/register", async (req, res) => {
+  try{
   const { name, email, password, phone, role } = req.body;
   if (!name || !email || !password){
     return res.json({
       data:null,
        message:"All required fields must be filled"
+    })
+ }
+    const existingUser=await User.findOne({email});
+    if(existingUser){
+      return res.json({
+        message:"User already exists",
+        data:null
+      })
+    }
+    const hashedPass=await bcrypt.hash(password, 10);
+
+    const user=new User({
+      name,
+      email,
+      password:hashedPass,
+      phone,
+      role
+    })
+    const savedUser=await user.save();
+    return res.json({
+      data:savedUser,
+      message:"User registered successfully"
+    })
+ 
+  }catch(e){
+    return res.json({
+      message:"error while registering",
+      error:e.message,
+      data:null
     })
   }
 });
