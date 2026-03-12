4ed0e61b0edd69a4dd3d4b0eaa2945bcccb80896
siddhikharde <siddhikharde2005@gmail.com>
2026-03-12 18:26:44 +0530

    mobgodb connected

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 const PORT=5000;
+
+const connectDB= async()=>{
+  const conn=await mongoose.connect(process.env.MONGODB_URI);
+  console.log("Mongodb connected...")
+}
 app.get("/", (req, res) => {
   res.send("RoomForU API Running");
 });
@@ -21,4 +29,5 @@ app.post("/register", (req, res) => {
 });
 app.listen(PORT, () => {
   console.log("Server running on port 5000");
+  connectDB();
 });
