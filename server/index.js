ac00a7a85d8c59d0722376a44648df5773d38bbe
siddhikharde <siddhikharde2005@gmail.com>
2026-03-13 11:13:10 +0530

    login api created

--- server/index.js
+++ server/index.js
@@ -2,8 +2,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import User from "./models/Users.js";
-import bcrypt from "bcryptjs"
+import { postRegister, postLogin } from "./controllers/Auth.js";
 
 dotenv.config();
 const app = express();
@@ -20,45 +19,9 @@ app.get("/", (req, res) => {
   res.send("RoomForU API Running");
 });
 
-app.post("/register", async (req, res) => {
-  try{
-  const { name, email, password, phone, role } = req.body;
-  if (!name || !email || !password){
-    return res.json({
-      data:null,
-       message:"All required fields must be filled"
-    })
- }
-    const existingUser=await User.findOne({email});
-    if(existingUser){
-      return res.json({
-        message:"User already exists",
-        data:null
-      })
-    }
-    const hashedPass=await bcrypt.hash(password, 10);
+app.post("/register", postRegister );
+app.post("/login", postLogin);
 
-    const user=new User({
-      name,
-      email,
-      password:hashedPass,
-      phone,
-      role
-    })
-    const savedUser=await user.save();
-    return res.json({
-      data:savedUser,
-      message:"User registered successfully"
-    })
- 
-  }catch(e){
-    return res.json({
-      message:"error while registering",
-      error:e.message,
-      data:null
-    })
-  }
-});
 app.listen(PORT, () => {
   console.log("Server running on port 5000");
   connectDB();
