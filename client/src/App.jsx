5e4a0951233c81d793cd6f6893217e7dc4f4c981
Shrushti <shrushtidhokchaule@gmail.com>
2026-03-20 17:05:19 +0530

    moved work from main to design-hom-page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,9 +10,15 @@ import Login from "./Pages/Login";
 import Slider from "./Pages/Slider";
 
 import Register from "./Pages/Register";
+<<<<<<< Updated upstream
 import Bookings from './Pages/Bookings';
 
 
+=======
+import About from './Pages/About';
+import Contacts from './Pages/contacts';
+import  BookingPage from './Pages/Bookings';
+>>>>>>> Stashed changes
 function App() {
   return (
     
@@ -27,8 +33,16 @@ function App() {
         <Route path="/" element={<Slider></Slider>}></Route>
 
         <Route path="/login" element={<Login></Login>}></Route>
+<<<<<<< Updated upstream
+=======
+        <Route path="/register" element={<Register></Register>}></Route>
+        <Route path="/roomdetail" element={<RoomDetail></RoomDetail>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/contacts" element={<Contacts></Contacts>}></Route>
+
+>>>>>>> Stashed changes
         <Route path="/rooms" element={<Rooms></Rooms>}></Route>
-        <Route path="/bookings" element={<Bookings></Bookings>}></Route>
+        <Route path="/book/:id" element={<BookingPage />} />
         <Route path="/roomsdetail" element={<RoomDetail></RoomDetail>}></Route>
 
         
--- client/src/Pages/Bookings.jsx
+++ client/src/Pages/Bookings.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { useParams } from "react-router-dom";
 
-function Bookings() {
+
+function BookingPage() {
+  const { id } = useParams();
+
   return (
-    <div>Bookings</div>
-  )
+    <div className="container py-5">
+      <h2>Booking for Room ID: {id}</h2>
+
+      <form style={{ maxWidth: "400px" }}>
+        <input type="text" placeholder="Your Name" className="form-control mb-3" />
+        <input type="date" className="form-control mb-3" />
+        <input type="date" className="form-control mb-3" />
+
+        <button className="btn btn-success">Confirm Booking</button>
+      </form>
+    </div>
+  );
 }
 
-export default Bookings
+export default BookingPage;
+
+  
