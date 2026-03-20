6d3cf9e9c90c2efca72fa35260caeeb2978a6417
siddhikharde <siddhikharde2005@gmail.com>
2026-03-20 18:56:09 +0530

    addProperty api updated

--- server/controllers/property.js
+++ server/controllers/property.js
@@ -2,56 +2,36 @@ import dotenv from "dotenv"
 import Property from "../models/Property.js";
 dotenv.config();
 
-const addProperty=async (req, res)=>{
-  try{
+const addProperty = async (req, res) => {
+  try {
 
-    const {title,location,rent,description} = req.body;
+    const { title, location, rent, description } = req.body;
+
+    const imageUrls = req.files.map(file => file.path);
 
     const property = new Property({
       title,
       location,
       rent,
       description,
-      owner:req.user.id
+      images: imageUrls,  
+      owner: req.user.id
     });
 
     await property.save();
 
     res.json({
-      message:"Property added successfully",
-      data:property
+      message: "Property added successfully",
+      data: property
     });
 
-  }catch(e){
-
-    res.json({
-      message:"Server error",
-      error:e.message
-    });
-
-  }
-}
-
-const getAllProperties= async (req, res)=>{
-             try {
-
-    const properties = await Property.find({ status: "approved" })
-      .populate("owner", "name email");
-
-    res.json({
-      message: "Properties fetched successfully",
-      data: properties
-    });
-
-  } catch (error) {
-
+  } catch (e) {
     res.json({
       message: "Server error",
-      error: error.message
+      error: e.message
     });
-
   }
-}
+};
 
 const searchProperties= async (req, res)=>{
   try {
